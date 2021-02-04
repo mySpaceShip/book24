9e50ed3c3de306a3e911e4bc282571c6b35de76f
Brusnitsyn Vyacheslav <brusnitsyn@squilla.io>
2021-02-04 22:59:52 +0500

    подключил форму поиска товаров к проекту

---
+++ src/app/BookApp.vue
@@ -0,0 +1,19 @@
+<template  lang="pug">
+  search-form
+
+
+</template>
+
+<script>
+import SearchForm from "@/components/SearchForm/SearchForm";
+export default {
+name: "BookApp",
+  components: {
+  SearchForm
+  },
+}
+</script>
+
+<style lang="scss">
+
+</style>
