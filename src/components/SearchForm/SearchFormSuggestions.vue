c6acba04bc554ef5b3b11c3cb86e842402d032d6
Brusnitsyn Vyacheslav <brusnitsyn@squilla.io>
2021-02-06 02:13:46 +0500

    вынес в отдельный компонент список поисковых запросов формы

---
+++ src/components/SearchForm/SearchFormSuggestions.vue
@@ -0,0 +1,95 @@
+<template lang="pug">
+  .b-24-search-suggestions
+    div(class='b24-search-suggestions__item'
+      v-for="item in suggestions.simple" :key="item.id"
+      :class="{'b24-search-suggestions__item--selected': item.id === selectedOfferId}")
+      a.b24-search-suggestions__link(:href="item.link") {{item.title}}
+    div(class=`b24-search-suggestions__item b24-search-suggestions__item--section`
+      v-for="item in suggestions.breadCrumbs" :key="item.id"
+      :class="{'b24-search-suggestions__item--selected': item.id === selectedOfferId}")
+      a(:href="item.link"
+        class="b24-search-suggestions__link") {{item.title}}
+      .b24-search-suggestions__breadcrumbs
+        a(class="b24-search-suggestions__link b24-search-suggestions__link--breadcrumb"
+          v-for="breadcrumb in item.meta.bread_crumb"
+          :key="breadcrumb.id"
+          :href="breadcrumb.url")
+          | {{breadcrumb.name}}
+    search-form-product(class="b24-search-suggestions__item"
+      v-for="(item, index) in suggestions.product"
+      :key="item.id"
+      :product="item"
+      :class=`{'b24-search-suggestions__item--selected': item.id === selectedOfferId,
+                'b24-search-suggestions__item--section': index === 0}`)
+</template>
+
+<script>
+import SearchFormProduct from "@/components/SearchForm/SearchFormProduct";
+export default {
+  name: "SearchFormSuggestions",
+  components: {
+    SearchFormProduct
+  },
+  props: {
+    selectedOfferId: {
+      type: String,
+      default: ''
+    },
+    suggestions: {
+      type: Object,
+      default: () => {}
+    },
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.b24-search-suggestions {
+
+  &__item {
+    padding: 14px 16px;
+    cursor: pointer;
+
+    &:hover {
+      background: #F5F6F9;
+    }
+
+    &--section {
+      border-top: 1px solid #C7CCD7;
+    }
+
+    &--selected {
+      background: #F5F6F9;
+    }
+  }
+
+  &__link {
+    color: #242424;
+    text-decoration: none;
+
+    &:hover, &:active {
+      color: lighten(#242424, 15%);
+    }
+
+    &--breadcrumb {
+      color:  #7D8290;
+
+      &:after {
+        content: '\00a0/\00a0';
+        color: #7D8290;
+      }
+
+      &:last-child {
+        &:after {
+          content: none;
+        }
+      }
+    }
+  }
+
+  &__breadcrumbs {
+    padding-top: 12px;
+  }
+}
+
+</style>
--- src/components/SearchForm/SearchOfferList.vue
+++ src/components/SearchForm/SearchOfferList.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
   .b24-search-offers
-    search-offer-history-phrases(
+    search-form-history-phrases(
       v-if="visibleHistoryAndPhrases"
       :history="history"
       :popular-phrases="popularPhrases"
@@ -8,41 +8,23 @@
       @selectItem="selectItem"
       @clearAllHistory="clearAllHistory"
       @removeHistoryItem="removeHistoryItem")
-    div(v-if="visibleSuggestions")
-      div.b24-search-offers__list
-        div(class=`b24-search-offers__item`
-            v-for="(item, index) in suggestions.simple" :key="index"
-            :class="{'b24-search-offers__item--main': item.id === selectedOfferId}")
-          a.b24-search-offers__link(:href="item.link") {{item.title}}
-      div.b24-search-offers__list
-        div(class=`b24-search-offers__item`
-            v-for="item in suggestions.breadCrumbs" :key="item.id"
-            :class="{'b24-search-offers__item--main': item.id === selectedOfferId}")
-          a(:href="item.link"
-            class="b24-search-offers__link") {{item.title}}
-          p.b24-search-offers__breadcrumbs
-            a(class="b24-search-offers__link b24-search-offers__link--breadcrumb"
-              v-for="breadcrumb in item.meta.bread_crumb"
-              :key="breadcrumb.id"
-              :href="breadcrumb.url")
-              | {{breadcrumb.name}}
-      div.b24-search-offers__list
-        search-offer-card(class="b24-search-offers__item"
-          v-for="item in suggestions.product"
-          :key="item.id"
-          :product="item"
-          :class="{'b24-search-offers__item--main': item.id === selectedOfferId}")
+    search-form-suggestions(
+      v-if="visibleSuggestions"
+      :suggestions="suggestions"
+      :selectedOfferId="selectedOfferId")
 </template>
 
 <script>
-import SearchOfferProduct from "@/components/SearchForm/SearchOfferProduct";
-import SearchOfferHistoryPhrases from "@/components/SearchForm/SearchOfferHistoryPhrases";
+import SearchOfferProduct from "@/components/SearchForm/SearchFormProduct";
+import SearchFormHistoryPhrases from "@/components/SearchForm/SearchFormHistoryPhrases";
+import SearchFormSuggestions from "@/components/SearchForm/SearchFormSuggestions";
 
 export default {
   name: "SearchOfferList",
   components: {
     SearchOfferProduct,
-    SearchOfferHistoryPhrases
+    SearchFormHistoryPhrases,
+    SearchFormSuggestions
   },
   props: {
     history: {
@@ -90,82 +72,8 @@ export default {
 .b24-search-offers {
   border: 1px solid #C7CCD7;
   background: #fff;
-  border-radius: 0px 0px 4px 4px;
+  border-radius: 0 0 4px 4px;
   box-shadow: 0 2px 8px 0 rgba(36,36,36,.25);
-
-  &__item {
-    padding: 8px 20px 8px 16px;
-    color: #242424;
-    font-size: 14px;
-    line-height: 16px;
-    font-family: 'Open Sans', sans-serif;
-    text-align: left;
-    cursor: pointer;
-
-    &:hover {
-      background: #F5F6F9;
-    }
-
-    &--main {
-      background: darken(#F5F6F9, 4%);
-      color: #7D8290;
-    }
-
-    &--flex-between {
-      display: flex;
-      justify-content: space-between;
-      align-items: center;
-    }
-  }
-
-  &__list {
-    border-bottom: 1px solid #C7CCD7;
-  }
-
-  &__link {
-    color: #242424;
-    text-decoration: none;
-
-    &:hover, &:active {
-      color: lighten(#242424, 15%);
-    }
-
-    &--breadcrumb {
-      color:  #7D8290;
-
-      &:after {
-        content: '\00a0/\00a0';
-        color: #7D8290;
-      }
-
-      &:last-child {
-        &:after {
-          content: none;
-        }
-      }
-    }
-  }
-
-  &__breadcrumbs {
-    padding-top: 8px;
-  }
-
-  &__clear-btn {
-    padding: 0;
-    border: none;
-    background: transparent;
-    cursor: pointer;
-    color: #7D8290;
-    font-family: 'Open Sans', sans-serif;
-
-    &:focus {
-      outline: none;
-    }
-
-    &:active, &:hover {
-      color: darken(#7D8290, 15%);
-    }
-  }
 }
 
 </style>
